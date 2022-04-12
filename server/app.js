910a87f96b6cbf654af6ee69e498c40492d601bc
RicardoMatiasPriou <74669252+RicardoMatiasPriou@users.noreply.github.com>
2022-04-12 17:26:25 -0300

    1212

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import postsRoutes from './routes/posts.routes.js'
 import fileUpload from "express-fileupload";
-
+import {dirname, join} from 'path'
+import {fileURLToPath} from 'url'
 
 const app = express()
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // middelwares
 app.use(express.json())
@@ -14,6 +16,7 @@ app.use(fileUpload({
 //routes
 app.use(postsRoutes)
 
+app.use(express.static(join(__dirname,'../client/build')))
 
 
 
